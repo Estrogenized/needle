7b54d86cc09ca21192deb6219e96521938ea5d7e
lumii500pg <blank1337@keemail.me>
2022-11-07 19:06:40 +0100

    added shadowing for public use

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    java
-    `java-library`
+    id("java")
+    id("java-library")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "moe.queery"
@@ -14,4 +15,15 @@ dependencies {
     implementation(group = "org.jetbrains", name = "annotations", version = "23.0.0")
     implementation(group = "it.unimi.dsi", name = "fastutil", version = "8.5.9")
     implementation(group = "com.nulab-inc", name="zxcvbn", version="1.7.0")
+}
+
+tasks {
+    java {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+        withSourcesJar()
+        build {
+            dependsOn.add(shadowJar)
+        }
+    }
 }
