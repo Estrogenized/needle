f17431c162516a23106e33fb3039404be600cf9e
lumii500pg <blank1337@keemail.me>
2022-11-15 21:37:14 +0100

    added sub-method for arrays

--- src/main/java/moe/queery/needle/utils/Arrays.java
+++ src/main/java/moe/queery/needle/utils/Arrays.java
@@ -82,4 +82,84 @@ public final class Arrays {
         }
         return false;
     }
+
+    public static boolean[] sub(final boolean[] array, final int start, final int end) {
+        final int min = Math.min(start, end), max = Math.max(start, end);
+        final int length = (max - min) + 1;
+        final boolean[] objs = new boolean[length];
+        System.arraycopy(array, min, objs, max, length);
+        return objs;
+    }
+
+    public static short[] sub(final short[] array, final int start, final int end) {
+        final int min = Math.min(start, end), max = Math.max(start, end);
+        final int length = (max - min) + 1;
+        final short[] objs = new short[length];
+        System.arraycopy(array, min, objs, max, length);
+        return objs;
+    }
+
+    public static int[] sub(final int[] array, final int start, final int end) {
+        final int min = Math.min(start, end), max = Math.max(start, end);
+        final int length = (max - min) + 1;
+        final int[] objs = new int[length];
+        System.arraycopy(array, min, objs, max, length);
+        return objs;
+    }
+
+    public static long[] sub(final long[] array, final int start, final int end) {
+        final int min = Math.min(start, end), max = Math.max(start, end);
+        final int length = (max - min) + 1;
+        final long[] objs = new long[length];
+        System.arraycopy(array, min, objs, max, length);
+        return objs;
+    }
+
+    public static float[] sub(final float[] array, final int start, final int end) {
+        final int min = Math.min(start, end), max = Math.max(start, end);
+        final int length = (max - min) + 1;
+        final float[] objs = new float[length];
+        System.arraycopy(array, min, objs, max, length);
+        return objs;
+    }
+
+    public static double[] sub(final double[] array, final int start, final int end) {
+        final int min = Math.min(start, end), max = Math.max(start, end);
+        final int length = (max - min) + 1;
+        final double[] objs = new double[length];
+        System.arraycopy(array, min, objs, max, length);
+        return objs;
+    }
+
+    public static byte[] sub(final byte[] array, final int start, final int end) {
+        final int min = Math.min(start, end), max = Math.max(start, end);
+        final int length = (max - min) + 1;
+        final byte[] objs = new byte[length];
+        System.arraycopy(array, min, objs, max, length);
+        return objs;
+    }
+
+    public static char[] sub(final char[] array, final int start, final int end) {
+        final int min = Math.min(start, end), max = Math.max(start, end);
+        final int length = (max - min) + 1;
+        final char[] objs = new char[length];
+        System.arraycopy(array, min, objs, max, length);
+        return objs;
+    }
+
+    public static String[] sub(final String[] array, final int start, final int end) {
+        final int min = Math.min(start, end), max = Math.max(start, end);
+        final int length = (max - min) + 1;
+        final String[] objs = new String[length];
+        System.arraycopy(array, min, objs, max, length);
+        return objs;
+    }
+
+    public static Object[] sub(final Object[] array, final int start, final int end) {
+        final int min = Math.min(start, end), max = Math.max(start, end);
+        final int length = (max - min) + 1;
+        final Object[] objs = new Object[length];
+        System.arraycopy(array, min, objs, max, length);
+        return objs;
+    }
 }
