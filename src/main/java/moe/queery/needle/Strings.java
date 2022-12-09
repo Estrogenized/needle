864235eb63e6b48df5337d1c3e1090f11dfc5e00
lumii500pg <blank1337@keemail.me>
2022-12-09 22:56:55 +0100

    ✨ added smallestOf in Strings

--- src/main/java/moe/queery/needle/Strings.java
+++ src/main/java/moe/queery/needle/Strings.java
@@ -3,21 +3,31 @@ package moe.queery.needle;
 import java.util.List;
 
 public final class Strings {
-
     // @formatter:off
     private static final String UPPER_CHARS   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String LOWER_CHARS   = "abdcefghijklmnopqrstuvwxyz";
     private static final String SPECIAL_CHARS = "!@#$%&*()_+-=[]|,./?><";
     private static final String NUMBER_CHARS  = "0123456789";
-
-    public static String longestOf(final List<String> strings) { return longestOf(strings.toArray(new String[0])); }
     // @formatter:on
 
+    // @formatter:off
+    public static String longestOf(final List<String> strings) { return longestOf(strings.toArray(new String[0])); }
+    // @formatter:on
     public static String longestOf(final String... strings) {
         String longestString = "";
         int max = 0;
         for (final String string : strings) if (string.length() > max) max = (longestString = string).length();
         return longestString;
+    }
+
+    // @formatter:off
+    public static String smallestOf(final List<String> strings) { return smallestOf(strings.toArray(new String[0])); }
+    // @formatter:on
+    public static String smallestOf(final String... strings) {
+        String smallestString = "";
+        int min = Integer.MAX_VALUE;
+        for (final String string : strings) if (string.length() < min) min = (smallestString = string).length();
+        return smallestString;
     }
 
     public static String getLowerChars() {
