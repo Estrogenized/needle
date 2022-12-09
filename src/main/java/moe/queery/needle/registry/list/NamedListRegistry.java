95269231b2a9103f49a654ca5c1ce802c7393e1b
lumii500pg <blank1337@keemail.me>
2022-12-09 22:49:17 +0100

    ✨ renamed the method getByName properly to getValueByName

--- src/main/java/moe/queery/needle/registry/list/NamedListRegistry.java
+++ src/main/java/moe/queery/needle/registry/list/NamedListRegistry.java
@@ -9,7 +9,7 @@ public class NamedListRegistry<V extends INameable> extends ListRegistry<V> {
     }
 
     @SuppressWarnings("unchecked") // it's checked ffs ._.
-    public <T extends V> @Nullable T getByName(final String name, final boolean ignoreCase) {
+    public <T extends V> @Nullable T getValueByName(final String name, final boolean ignoreCase) {
         for (final V value : this.getValues()) {
             if (!(ignoreCase ? value.getName().equalsIgnoreCase(name) : value.getName().equals(name))) continue;
             return (T) value;
